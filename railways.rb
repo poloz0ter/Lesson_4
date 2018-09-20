163a725f9d989cde0f5121f32d6feb6405f32750
poloz0ter <polozoter@gmail.com>
2018-09-20 16:54:08 +1100

    Complete

---
+++ railways.rb
@@ -0,0 +1,94 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def take_train(train)
+    @trains << train
+  end
+
+  def trains_by_type(type)
+    @trains.select { |train| train.type == type }
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+end
+
+class Route
+  attr_reader :stations
+
+  def initialize(start, finish)
+    @stations = [start, finish]
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    @stations.delete(station)
+  end
+end
+
+class Train
+  attr_reader :number, :type, :wagon_count, :speed
+
+  def initialize(number, type, wagon_count)
+    @number = number
+    @type = type
+    @wagon_count = wagon_count
+    @speed = 0
+    @station_index = 0
+  end
+
+  def speed_up
+    @speed += 20
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def add_wagon
+    @wagon_count += 1 if @speed.zero?
+  end
+
+  def delete_wagon
+    @wagon_count -= 1 if @speed.zero? && @wagon_count > 0
+  end
+
+  def route=(route)
+    @route = route
+    @station_index = 0
+    current_station.take_train(self)
+  end
+
+  def go_forward
+    current_station.send_train(self)
+    @station_index += 1
+    current_station.take_train(self)
+  end
+
+  def go_backward
+    current_station.send_train(self)
+    @station_index -= 1
+    current_station.take_train(self)
+  end
+
+  def current_station
+    @route.stations[@station_index]
+  end
+
+  def next_station
+    @route.stations[@station_index + 1]
+  end
+
+  def previous_station
+    @route.stations[@station_index - 1]
+  end
+end
